191d49b4b95ce43c7c55540c3fddb207b170762d
R,H <162550688+RH0099@users.noreply.github.com>
2025-02-08 16:18:33 +0600

    install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 echo "🔹 Installing dependencies..."
-pkg update -y && pkg upgrade -y
-pkg install git -y
-pkg instoll python -y
-php curl wget -y
+pkg update -y && pkg upgrade -y &&
+pkg install git -y &&
+pkg instoll python -y &&
+pkg install php -y &&
+pkg install curl -y &&
+pkg install wget -y 
 echo "✅ Installation complete!"
